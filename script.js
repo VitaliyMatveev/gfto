42647289d46e11633b47a5d4cc1cc0b2608a348f
Vitaliy Matveev <vmatveev@itgroup.lc>
2015-11-02 21:35:05 +0300

    refactoring

--- script.js
+++
@@ -1,9 +0,0 @@
- $(function() {
-	$(".menu a").on("click", function (e) {
-    console.log(this,e);
-    if($(e.currentTarget).parent().parent().hasClass("lvl_one")){
-      $(".lvl_one>li").removeClass("active");
-      $(this).parent().addClass("active");  
-    }        
-  })
- });
